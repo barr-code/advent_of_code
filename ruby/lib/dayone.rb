0cf236b963fbb7883ec77a42d42d197a2ba1736f
barr-code <vb329@georgetown.edu>
2015-12-07 20:48:30 +0000

    refactoring

--- ruby/lib/dayone.rb
+++ ruby/lib/dayone.rb
@@ -1,27 +1,24 @@
 class DayOne
   def evaluate(string)
-    if string == "("
-      1
-    elsif string == ")"
-      -1
-    end
+    string == "(" ? 1 : -1
+  end
+
+  def convert_string_to_number_array(string)
+    string.split('').map{|v| evaluate(v)}
   end
 
   def add_string(string)
-    values = string.split('')
-    values = values.map {|v| evaluate(v)}
+    values = convert_string_to_number_array(string)
     values.inject(:+)
   end
 
   def find_position_of_minus_one(string)
     floor = 0
     position = nil
-    values = string.split('').map{|v| evaluate(v)}
+    values = convert_string_to_number_array(string)
     values.each_with_index do |value, index|
       floor += value
-      if floor == -1 && !position
-        position = index + 1
-      end
+      position = index + 1 if floor == -1 && !position
     end
     return position
   end
